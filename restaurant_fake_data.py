700298b82c0d0932e0eb6a30d70c77fb5cd49512
Felix Li <liyb.gz@gmail.com>
2017-08-03 18:24:44 +0800

    Add fake data

---
+++ restaurant_fake_data.py
@@ -0,0 +1,38 @@
+test_res = {
+	'id': 1,
+	'name': 'Test Restaurant',
+	'description': 'Test Restaurant Description\n Lorem ipsum dolor sit amet, \
+	consectetur adipisicing elit. Voluptas aperiam quibusdam natus, sint nulla\
+	 deserunt facilis ut unde, delectus temporibus atque, impedit qui quisquam\
+	 quod laborum quas, odio nemo soluta.'
+}
+
+test_reses = [test_res]
+
+test_item_1 = {
+	'id': 1,
+	'name': 'Test Dish',
+	'description': 'Test Dish Description. \n Lorem ipsum dolor sit amet, \
+	consectetur adipisicing elit. Totam veniam, corporis, ullam omnis culpa \
+	reprehenderit delectus explicabo id ducimus, vero amet corrupti placeat \
+	architecto quaerat aspernatur iusto in facere expedita!',
+	'course': 'Test Course',
+	'price': '$29.99',
+	'restaurant_id': 1,
+	'restaurant': test_res
+}
+
+test_item_2 = {
+	'id': 2,
+	'name': 'Test Dish 2',
+	'description': 'Test Dish 2 Description. \n Lorem ipsum dolor sit amet, \
+	consectetur adipisicing elit. Totam veniam, corporis, ullam omnis culpa \
+	reprehenderit delectus explicabo id ducimus, vero amet corrupti placeat \
+	architecto quaerat aspernatur iusto in facere expedita!',
+	'course': 'Test Course',
+	'price': '$30.00',
+	'restaurant_id': 2,
+	'restaurant': None
+}
+
+test_items = [test_item_1, test_item_2]
