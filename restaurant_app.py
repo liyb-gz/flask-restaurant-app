737ba74044b92af10721540955990a3b69e7ba59
Felix Li <liyb.gz@gmail.com>
2017-08-03 18:28:05 +0800

    change variable name from restaurants_id to restaurant_id

--- restaurant_app.py
+++ restaurant_app.py
@@ -1,39 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+from restaurant_fake_data import *
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/restaurants/")
 def list_restaurants():
-    return "list_restaurants"
+    return render_template('restaurants.html', restaurants = test_reses)
 
 @app.route("/restaurants/add")
 def add_restaurant():
 	return "add_restaurant"
 
-@app.route("/restaurants/<int:restaurants_id>/edit")
+@app.route("/restaurants/<int:restaurant_id>/edit")
 def edit_restaurant():
 	return "edit_restaurant"
 
-@app.route("/restaurants/<int:restaurants_id>/delete")
+@app.route("/restaurants/<int:restaurant_id>/delete")
 def delete_restaurant():
 	return "delete_restaurant"
 
-@app.route("/restaurants/<int:restaurants_id>/")
-@app.route("/restaurants/<int:restaurants_id>/menu/")
-def list_menu_item(restaurants_id):
+@app.route("/restaurants/<int:restaurant_id>/")
+@app.route("/restaurants/<int:restaurant_id>/menu/")
+def list_menu_item(restaurant_id):
 	return "list_restaurant_menu"
 
-@app.route("/restaurants/<int:restaurants_id>/menu/add")
-def add_menu_item(restaurants_id):
+@app.route("/restaurants/<int:restaurant_id>/menu/add")
+def add_menu_item(restaurant_id):
 	return "add_menu_item"
 
-@app.route("/restaurants/<int:restaurants_id>/menu/<int:menu_item_id>/")
-@app.route("/restaurants/<int:restaurants_id>/menu/<int:menu_item_id>/edit")
-def edit_menu_item(restaurants_id, menu_item_id):
+@app.route("/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/")
+@app.route("/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/edit")
+def edit_menu_item(restaurant_id, menu_item_id):
 	return "edit_menu_item"
 
-@app.route("/restaurants/<int:restaurants_id>/menu/<int:menu_item_id>/delete")
-def delete_menu_item(restaurants_id, menu_item_id):
+@app.route("/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/delete")
+def delete_menu_item(restaurant_id, menu_item_id):
 	return "delete_menu_item"
 
 
