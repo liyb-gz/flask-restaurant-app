3f956a11f5d34469402983e9efa7c4ecd39a284f
Felix Li <liyb.gz@gmail.com>
2017-08-08 13:22:45 +0800

    Add serialize for database

--- database.py
+++ database.py
@@ -13,6 +13,14 @@ class Restaurant(Base):
 	name = Column(String(80), nullable = False)
 	description = Column(String(250))
 
+	@property
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'description': self.description
+		}
+
 class MenuItem(Base):
 	__tablename__ = "menu_item"
 
@@ -24,6 +32,16 @@ class MenuItem(Base):
 
 	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
+
+	@property
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'description': self.description,
+			'course': self.course,
+			'price': self.price
+		}
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
